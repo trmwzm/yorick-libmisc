9e897e00ba0ea4d533deeccbb858337ee83bf3eb
Thierry R Michel <trm@dhcp-137-78-253-235.jpl.nasa.gov>
2024-04-01 12:34:35 -0700

    yx add verbose

---
+++ examples/yx-1.mk
@@ -0,0 +1,11 @@
+MAKEFLAGS += --warn-undefined-variables
+SHELL := bash
+.SHELLFLAGS := -eu -o pipefail -c
+.DEFAULT_GOAL := all
+.DELETE_ON_ERROR:
+.SUFFIXES:
+
+all: jk.txt
+
+jk.txt:
+	yx 'a= exp(1.0)' 'f= open("jk.txt","w")' 'nb= write(f,a)' 'return nb==0'
